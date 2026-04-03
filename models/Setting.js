ff9fe9d3c96ef7931679e38ca2746300065588c8
fdyst <rtoraymankentos@gmail.com>
2026-04-03 16:28:08 +0700

    ✨ FEAT: Create Setting model for Super Admin Dashboard

--- models/Setting.js
+++ models/Setting.js
@@ -1,6 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Setting', {
-        key: { type: DataTypes.STRING, primaryKey: true },
-        value: { type: DataTypes.STRING }
-    }, { timestamps: false });
-};
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Setting = sequelize.define('Setting', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  botToken: {
+    type: DataTypes.STRING,
+    allowNull: true // Boleh kosong awal-awal, nanti diisi dari dashboard lu
+  },
+  maintenanceMode: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  tableName: 'settings',
+  timestamps: true
+});
+
+module.exports = Setting;
