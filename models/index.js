ba4c9ec1ce39971912f2cfeaf848e524ec986f08
fdyst <rtoraymankentos@gmail.com>
2026-04-03 15:53:45 +0700

    🛠️ FIX: ReferenceError Sequelize and sync models

--- models/index.js
+++ models/index.js
@@ -1,17 +1,33 @@
+const { Sequelize } = require('sequelize');
 const sequelize = require('../db');
 
-// Import semua model di sini
+// Import semua model
 const User = require('./User');
 const Product = require('./Product');
 const Transaction = require('./Transaction');
-// ... tambahin model lainnya kalau ada ...
+const Deposit = require('./Deposit');
+const CustomBot = require('./CustomBot');
+const Guild = require('./Guild');
+const Setting = require('./Setting');
+const Subscription = require('./Subscription');
 
+// Masukin ke objek db
 const db = {
   sequelize,
   Sequelize,
   User,
   Product,
-  Transaction
+  Transaction,
+  Deposit,
+  CustomBot,
+  Guild,
+  Setting,
+  Subscription
 };
+
+// Sinkronisasi database
+db.sequelize.sync({ alter: true })
+  .then(() => console.log('✅ Semua tabel database berhasil disinkronkan!'))
+  .catch(err => console.error('❌ Gagal sinkronisasi database:', err));
 
 module.exports = db;
