9d27d320b09d59978f49b53d56e9268777485871
fdyst <rtoraymankentos@gmail.com>
2026-04-05 09:13:28 +0700

    🩹 FIX: Final URL sync for CORS and Auth Redirect

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const app = express();
 app.set('trust proxy', 1);
 app.use(cors({
-  origin: ['https://sultan.vercel.app', 'http://localhost:5173'], // Ganti sultan.vercel.app sama URL asli lu
+  origin: ['https://frontend-sultan.vercel.app', 'http://localhost:5173'], // Ganti frontend-sultan.vercel.app sama URL asli lu
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
